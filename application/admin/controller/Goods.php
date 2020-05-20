52f6fad9310ffcb3999263d4cb503b0d032777c2
parasol <619372857@qq.com>
2020-05-20 17:10:24 +0800

    添加商品页面

--- application/admin/controller/Goods.php
+++ application/admin/controller/Goods.php
@@ -4,7 +4,48 @@
 namespace app\admin\controller;
 
 
-class Goods
+use parasol\DataEdit;
+
+class Goods extends Base implements DataEdit
 {
 
+    /**
+     * @return mixed
+     */
+    public function lists_index()
+    {
+        return $this->fetch();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function lists()
+    {
+        // TODO: Implement lists() method.
+    }
+
+    /**
+     * @return mixed
+     */
+    public function add_index()
+    {
+        // TODO: Implement add_index() method.
+    }
+
+    /**
+     * @return mixed
+     */
+    public function del()
+    {
+        // TODO: Implement del() method.
+    }
+
+    /**
+     * @return mixed
+     */
+    public function edit()
+    {
+        // TODO: Implement edit() method.
+    }
 }
